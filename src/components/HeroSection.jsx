cfe6cd569435f79efe9e689e07f20e87af3830a9
Dev Punjabi <dev.punjabi@mail.utoronto.ca>
2024-01-17 17:18:15 -0500

    Hero & Footer Context Update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ function HeroSection() {
                     Hi, Welcome To My Portfolio! 
                 </h1>
                 <p className='text-md md:text-xl max-w-lg mb-3 text-gray-600 dark:text-gray-300'> 
-                I'm Dev, a 3rd-year Computer Science student at the University of Toronto Mississauga enrolled in the PEY program. I'm actively seeking a 12-16 month internship starting in summer 2024.
+                I'm Dev, a 3rd-year Computer Science student at the University of Toronto Mississauga enrolled in the PEY program. Currently, I am minoring in mathematical sciences & buiness with interests in FinTech, data analytics & AI.
                 </p>
                 <a href="#projects" className='inline-block px-8 py-3 border border-transparent text-base font-medium text-gray-100 bg-sky-600 hover:bg-sky-700 md:text-md'> 
                 See My Projects
